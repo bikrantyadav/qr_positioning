7caaec8e436176e012f2f1268acab2af077b545d
bikrantyadav <learningbikrantyadav@gmail.com>
2026-03-29 12:34:45 +0300

    Add files via upload

--- 2506263.js
+++ 2506263.js
@@ -21,20 +21,6 @@ function startScanner() {
         function (text) {
             const place = JSON.parse(text);
             showMarkerAt(place.top, place.left);
-
-            document.getElementById("itemName").innerText = "Name: " + item.name;
-
-            document.getElementById("itemStatus").innerText =
-                "In store: " + (item.in_store ? "Yes" : "No");
-
-            document.getElementById("itemPrice").innerText =
-                "Price: €" + item.price;
-
-
-
-
-
-
             toggleScanner();
         }
     ).catch(function (err) {
